986d851c3623b85e7f9ea151f634f976781722b5
obaadz <obada347@gmail.com>
2024-01-23 23:49:31 +0200

    Add missing 'await' on user get informations api

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -70,7 +70,7 @@ export const userSetContacts = asyncHandler(
 
 export const userGetInformations = asyncHandler(
   async (req: RequestProtectMW<any, any, { contacts: string[] }>, res: Response) => {
-    req.dbUser.populate("contacts", "-password");
+    await req.dbUser.populate("contacts", "-password");
 
     req.dbUser = req.dbUser.toJSON();
 
